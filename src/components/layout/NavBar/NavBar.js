5484adeadc3dbf24656a71c8ec0fe82f9c9077b8
Max Sealey <maxsealey@users.noreply.github.com>
2022-06-23 12:14:57 -0700

    created layout components and scss stylesheets

---
+++ src/components/layout/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './navBar.scss'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div className='navbar'></div>
+    )
+  }
+}
---
+++ src/components/layout/Pagination/Pagination.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './pagination.scss'
+
+export default class Pagination extends Component {
+  render() {
+    return (
+      <div className='pagination'></div>
+    )
+  }
+}
