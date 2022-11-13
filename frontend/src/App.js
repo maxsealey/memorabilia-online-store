c81cca26e845c1b9f753f8fdd87ccb177dc10422
Max Sealey <maxsealey@users.noreply.github.com>
2022-11-12 19:18:34 -0800

    implemented react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import NavBar from './components/layout/NavBar';
+import Home from './components/pages/Home/Home';
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path = "/" component={Home} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
