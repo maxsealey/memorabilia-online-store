1a4f2f32cc22e0f4e65a7599d733e1f230e83988
Max Sealey <maxsealey@users.noreply.github.com>
2022-12-30 23:51:46 -0800

    Added description, condition fields, and image links hosted by cloudinary

--- src/data/products.js
+++ src/data/products.js
@@ -6,7 +6,9 @@ const products = [
         category: ["sports", "baseball"],
         authenticated:"yes",
         price: 199.99,
-        image: ""
+        image: "https://res.cloudinary.com/dcjlov2t7/image/upload/v1672472220/shop_samples/ichiro_nfetbe.jpg",
+        desc: "Signed Ichiro Mariners jersey",
+        condition: "mint"
     },
     {
         id:2,
@@ -15,7 +17,9 @@ const products = [
         category: ["sports", "football"],
         authenticated:"yes",
         price: 99.99,
-        image: ""
+        image: "https://res.cloudinary.com/dcjlov2t7/image/upload/v1672472220/shop_samples/largent_xuyx7x.jpg",
+        desc: "Signed Steve Largent Football",
+        condition: "very good"
     },
     {
         id:3,
@@ -24,7 +28,9 @@ const products = [
         category: ["sports", "football"],
         authenticated:"yes",
         price: 199.99,
-        image: ""
+        image: "https://res.cloudinary.com/dcjlov2t7/image/upload/v1672472221/shop_samples/prime_wzrj3y.jpg",
+        desc: "Signed Primetime Sanders helmet",
+        condition: "mint"
     },
     {
         id:4,
@@ -33,16 +39,20 @@ const products = [
         category: ["history", "politics"],
         authenticated:"yes",
         price: 599.99,
-        image: ""
+        image: "https://res.cloudinary.com/dcjlov2t7/image/upload/v1672472220/shop_samples/franklin_mgwkkx.webp",
+        desc: "Ben Franklin's quill",
+        condition: "average"
     },
     {
         id:5,
-        item:"Used \"Snot-rag\"",
-        name:"Justin Bieber",
+        item:"Signed Guitar",
+        name:"Jimi Hendrix",
         category: ["celebrity", "personal"],
-        authenticated:"no",
-        price: 1299.99,
-        image: ""
+        authenticated:"yes",
+        price: 12950,
+        image: "https://res.cloudinary.com/dcjlov2t7/image/upload/v1672472470/shop_samples/vpwktyxrw2kwmj8dhkwr_xqvklg.webp",
+        desc: "Acoustic Guitar signed by Jimi Hendrix",
+        condition: "very good"
     }
 ]
 
